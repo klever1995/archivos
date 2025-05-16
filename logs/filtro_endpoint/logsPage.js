cff130704b05cf6aa7034f4e2f6c2abae8aa0a1a
klever1995 <150808199+klever1995@users.noreply.github.com>
2025-05-16 15:37:32 -0500

    Create logsPage.js

---
+++ logs/filtro_endpoint/logsPage.js
@@ -0,0 +1,11 @@
+// Forma correcta (opción 1):
+const LogsPage = () => {
+    return (
+      <div>
+        <h1>hola desde los logs</h1>
+        {/* Tu contenido aquí */}
+      </div>
+    );
+  };
+  export default LogsPage;
+  
