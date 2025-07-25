5a7e8dcc423aa4b81f42e9e890447c4fa0b202db
klever1995 <150808199+klever1995@users.noreply.github.com>
2025-07-25 15:34:43 -0500

    Create loLogsremotos.py

---
+++ logs/filtro_25-07-2025/loLogsremotos.py
@@ -0,0 +1,19 @@
+from config import db
+
+class loLogsremotos(db.Model):
+    __tablename__ = 'LO_LOGSREMOTOS'  # ¡Mayúsculas como en tus otros modelos!
+    
+    idLogRemoto = db.Column(db.Integer, primary_key=True)
+    idEmpresa = db.Column(db.Integer, db.ForeignKey('AS_EMPRESA.idEmpresa'), nullable=False)
+    idServidor = db.Column(db.Integer, db.ForeignKey('LO_SERVIDORES.idServidor'), nullable=False)
+    idAuditoria = db.Column(db.Integer, db.ForeignKey('LO_PROCESOS.idAuditoria'), nullable=False)
+    fechaCreacion = db.Column(db.DateTime, nullable=False)
+    nivel = db.Column(db.String(10), nullable=False)
+    mensaje = db.Column(db.Text)
+    categoria = db.Column(db.String(100))
+    ocurrencias = db.Column(db.Integer, default=1)
+
+    # Relaciones IDÉNTICAS a tus modelos funcionales (usando strings)
+    empresa = db.relationship('asEmpresa', backref='logsremotos')  # 1. String 'asEmpresa'
+    servidor = db.relationship('loServidores', foreign_keys=[idServidor], backref='logsremotos')  # 2. String 'loServidores'
+    proceso = db.relationship('LoProcesos', foreign_keys=[idAuditoria], backref='logsremotos')  # 3. String 'LoProcesos'
