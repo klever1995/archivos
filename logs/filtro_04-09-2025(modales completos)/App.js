d263be069ae6fff0b79f36d3806fbacd45ecd57a
klever1995 <150808199+klever1995@users.noreply.github.com>
2025-09-04 16:08:15 -0500

    Create App.js

---
+++ logs/filtro_04-09-2025(modales completos)/App.js
@@ -0,0 +1,90 @@
+import React from "react";
+import { Route, Routes, Navigate, Link } from "react-router-dom";
+import { AuthProvider, useAuth } from "./AuthProvider"; 
+import Login from "./login";
+import AdministraUsuariosPantalla from "./AdministraUsuariosPantalla";
+import AdministraRolesPantalla from "./AdministraRolesPantalla";
+import DatosAlmacenadosPantalla from "./DatosAlmacenadosPantalla";
+import CargarDatosPantalla from "./CargarDatosPantalla";
+import AdministraEmpresaPantalla from "./AdministraEmpresaPantalla";
+import AdministraPruebasPantalla from "./AdministraPruebasPantalla";
+import InicioPantalla from "./InicioPantalla";
+import MultiLogProcessor from './logs/MultiLogProcessor';
+import TableroLogs from "./logs/TableroLogs.js";
+import { LogsProvider } from "./logs/LogsContext"; 
+import { WebSocketProvider } from "./logs/WebsocketContext"; 
+import { ModalLogsCacheProvider } from "./logs/ModalLogsContext"; 
+import "./App.css";
+
+// Componente para proteger rutas
+const ProtectedRoute = ({ element }) => {
+  const { user, isInitialized } = useAuth();
+
+  if (!isInitialized) {
+    return <div>Cargando...</div>;
+  }
+
+  return user ? element : <Navigate to="/login" replace />;
+};
+
+// Sidebar
+const Sidebar = () => {
+  const { user, logout, isAdmin } = useAuth();
+  if (!user) return null;
+
+  return (
+    <div>
+      <div>
+        <label className="bienvenido">Bienvenido, {user.name}</label>
+      </div>
+      <div className="menu">
+        <nav>
+          <ul>
+            <li><Link to="/">Inicio</Link></li>
+            <li><Link to="/datos-almacenados">Bases de conocimiento</Link></li>
+            {isAdmin && (
+              <>
+                <li><Link to="/usuarios">Gestión de usuarios</Link></li>
+                <li><Link to="/empresa">Configuración general</Link></li>
+                <li><Link to="/administra-pruebas">Pruebas</Link></li>
+              </>
+            )}
+            <li><Link to="/login" onClick={logout}>⏻ Salir</Link></li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  );
+};
+
+function App() {
+  return (
+    <AuthProvider>
+      <WebSocketProvider idEmpresa={1}>
+        <ModalLogsCacheProvider> 
+          <LogsProvider>
+            <div>
+              <div className="header">
+                <Sidebar />
+              </div>
+
+              <Routes>
+                <Route path="/login" element={<MultiLogProcessor />} /> 
+                <Route path="/tablero-logs" element={<TableroLogs />} />
+                <Route path="/usuarios" element={<ProtectedRoute element={<AdministraUsuariosPantalla />} />} />
+                <Route path="/roles" element={<ProtectedRoute element={<AdministraRolesPantalla />} />} />
+                <Route path="/datos-almacenados" element={<ProtectedRoute element={<DatosAlmacenadosPantalla />} />} />
+                <Route path="/cargar-datos" element={<ProtectedRoute element={<CargarDatosPantalla />} />} />
+                <Route path="/empresa" element={<ProtectedRoute element={<AdministraEmpresaPantalla />} />} />
+                <Route path="/administra-pruebas" element={<ProtectedRoute element={<AdministraPruebasPantalla />} />} />
+                <Route path="/" element={<ProtectedRoute element={<InicioPantalla />} />} />
+              </Routes>
+            </div>
+          </LogsProvider>
+        </ModalLogsCacheProvider> 
+      </WebSocketProvider>
+    </AuthProvider>
+  );
+}
+
+export default App;
